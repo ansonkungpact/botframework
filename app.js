f175dca9f36d1fbc35524b587ac791428dfd6698
Anson Kung <anson.kung@pactera.com>
2017-04-06 11:25:47 +0800

    show json in log

--- app.js
+++ app.js
@@ -47,7 +47,13 @@ dialog.matches('greetings', [
 			// session.send(session.message.address.from);
 			// session.send(session.message.address.recipient);
 			delete session.message.address['user'];
+			console.log('-------------------');
 			console.log(session.message.address);
+			console.log('-------------------');
+			session.message.address.from.id = 'U4TUXVBUZ:T4ULM7F0U';
+			session.message.address.from.name = 'anson.kung';
+			console.log(session.message.address);
+			console.log('-------------------');
 		}
     },
 ]);
